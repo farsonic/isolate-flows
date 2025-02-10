e965c9926f88ed96617662ead7be6044fe5227f3
Francois Prowse <fp@prowsehouse.com>
2025-02-10 15:18:09 +1000

    Update start_vms.sh

--- start_vms.sh
+++ start_vms.sh
@@ -11,12 +11,13 @@ VM2_IMAGE="/var/tmp/vm2.img"
 CONFIG_SCRIPT="/home/pensando/isolate-flows/configure-vm.sh"
 VM1_XML="/home/pensando/isolate-flows/vm1.xml"
 VM2_XML="/home/pensando/isolate-flows/vm2.xml"
-MAC_ADDRESS="00:02:01:01:01:AA"  # Replace with actual MAC address
+MAC_ADDRESS="00:02:01:01:01:aa"  # Replace with actual MAC address
 
 # Make configure script executable
 chmod +x "$CONFIG_SCRIPT"
 
 # Download Ubuntu cloud image
+sudo rm "$IMAGE_PATH"
 sudo wget -P /var/tmp/ "$IMAGE_URL"
 
 # Set root password in the cloud image
